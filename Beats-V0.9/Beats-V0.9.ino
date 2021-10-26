70c935714fee969ca1b92413c206c817a7402958
Andy <acobley@computing.dundee.ac.uk>
2021-10-26 16:39:49 +0100

    added start/stop button and reset

--- Beats-V0.9/Beats-V0.9.ino
+++ Beats-V0.9/Beats-V0.9.ino
@@ -8,17 +8,23 @@
 const byte clockIn = 2;
 const byte GateKnob = A7;
 const byte playLED = 5;
+const byte playBut = 4;
 const byte reset = 3;
 const byte dirSW1 = 6;
 const byte dirSW2 = 7;
 
-const byte Muxs[] = {11, 12, 13};
 
+
+const byte Muxs[] = {11, 12, 13};
+const byte Running = 1;
+const byte Stopped = 2;
+
+byte State = Running;
 
 const byte Gates[] = {A0, A1, A2, A3, A4, A5, 8, 9};
 byte Pattern = 0;
 byte Patterns[3][16] = {{
-    0x01
+    0x01  //P0
     , 0xC0
     , 0x08
     , 0x94
@@ -38,27 +44,27 @@ byte Patterns[3][16] = {{
     , 0x89
     , 0x92
   },
-  { 0x01
-    , 0xC0
-    , 0x08
-    , 0x94
+  { 0xA3   //p1
+    , 0x80
+    , 0x80
+    , 0x85
 
-    , 0x01
-    , 0xA0
-    , 0x08
+    , 0x03
+    , 0x80
+    , 0x88
+    , 0x85
+
+    , 0x83
+    , 0x80
+    , 0x88
     , 0x84
 
-    , 0x01
-    , 0xc0
-    , 0x08
-    , 0x84
-
-    , 0x01
-    , 0xA0
+    , 0x81
+    , 0x81
     , 0x89
-    , 0x92
+    , 0x83
   },
-  { 0x83
+  { 0x83 //P2
     , 0x00
     , 0x80
     , 0x00
@@ -86,78 +92,6 @@ byte Patterns[3][16] = {{
 
 */
 
-int GateTime = 50; //How long the gate will be high.
-
-int n = 0;
-
-void clockChange() {
-  WritePattern(n);   // turn the LED on (HIGH is the voltage level)
-  n++;
-  if (n >= 8)
-    n = 0;
-
-}
-
-void clearGates() {
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(Gates[i], LOW);
-
-  }
-}
-
-void clearMuxs() {
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(Muxs[i], LOW);
-
-  }
-}
-
-/***  Debug variables
-
-*/
-
-//int MuxCount=0;
-
-void WriteMux(int n) {
-  //Debug
-  /*
-    clearMuxs();
-    digitalWrite(Muxs[MuxCount],HIGH);
-    MuxCount++;
-    if (MuxCount >=3)
-     MuxCount=0;
-    //endDebug
-
-  */
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(Muxs[i], (n & 0x01));
-    n >> 0x1;
-  }
-
-}
-
-
-
-
-void readGateLength() {
-  GateTime = (1024 - analogRead(GateKnob)) / 16; //0-> 125ms
-
-}
-
-void WritePattern(int n) {
-  byte Beats = Patterns[Pattern][n];
-  clearGates();
-  digitalWrite(playLED, HIGH);
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(Gates[i], (Beats & 0x01));
-    Beats = Beats >> 0x1;
-  }
-  WriteMux(n);
-  delay(GateTime);// wait gatetime Ms (this will effect the max tempo
-  clearGates();
-  digitalWrite(playLED, LOW);
-}
-
 byte States[] = {false, false, false, false, false, false, false, false};
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -171,6 +105,8 @@ void setup() {
   pinMode(playLED, OUTPUT);
   pinMode(dirSW1, INPUT);
   pinMode(dirSW2, INPUT);
+  pinMode(playBut, INPUT);
+  pinMode(reset, INPUT);
   for (int i = 0; i < 8; i++) {
     States[i] = !States[i];
     digitalWrite(Gates[i], States[i]);
@@ -188,6 +124,84 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(clockIn), clockChange, RISING);
 }
 
+int GateTime = 50; //How long the gate will be high.
+
+int n = 0;
+
+void clockChange() {
+  if (State == Running) {
+    WritePattern(n);   // turn the LED on (HIGH is the voltage level)
+    n++;
+    if (n >= 8)
+      n = 0;
+  }
+  if (digitalRead(reset)==LOW){
+    n=0;
+  }
+}
+
+void clearGates() {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(Gates[i], LOW);
+
+  }
+}
+
+void clearMuxs() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(Muxs[i], LOW);
+
+  }
+}
+
+
+void WriteMux(int n) {
+
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(Muxs[i], (n & 0x01));
+    n >> 0x1;
+  }
+
+}
+
+
+void readGateLength() {
+  GateTime = (1024 - analogRead(GateKnob));
+
+}
+
+int lastReading = LOW;
+void playButton() {
+  int reading = digitalRead(playBut);
+  if (reading != lastReading) {
+    if (reading == HIGH) {
+      if (State == Running) {
+        State = Stopped;
+      } else {
+        State = Running;
+      }
+
+    }
+  }
+  lastReading = reading;
+}
+
+void WritePattern(int n) {
+  byte Beats = Patterns[Pattern][n];
+  clearGates();
+  digitalWrite(playLED, HIGH);
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(Gates[i], (Beats & 0x01));
+    Beats = Beats >> 0x1;
+  }
+  WriteMux(n);
+  delay(GateTime);// wait gatetime Ms (this will effect the max tempo
+  clearGates();
+  digitalWrite(playLED, LOW);
+}
+
+
+
 byte PatternMap[] = {1, 0, 2}; //Used to convert Switch positions to pattern number (center is 0 on the switch)
 
 void getPatternNum() {
@@ -203,4 +217,5 @@ void loop() {
   // wait for a second
   readGateLength();
   getPatternNum();
+  playButton();
 }
